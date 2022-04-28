a7bb6c2dbfc485f2f2164e94b47c5b99dec79a66
sdaeric19 <78454351+sdaeric19@users.noreply.github.com>
2022-04-28 17:32:24 -0500

    Update documentation

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -4,6 +4,6 @@ from number_logic.src.number_logic import NumberLogic
 
 @pytest.fixture(scope="session")
 def number_logic():
-    """Create Write ElasticSearch object for R/W Operations"""
+    """Create NumberLogic object"""
     number_logic = NumberLogic()
     return number_logic
